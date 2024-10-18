708c023462fa23cbe399f83ae1698942b65cd043
BreidysG <Breidysgutierrez8@gmail.com>
2024-10-18 11:45:57 -0400

    Primer commit

---
+++ script.js
@@ -0,0 +1,183 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const titleInit = document.getElementById("titleInit");
+    const paragraphInit = document.getElementById("paragraphInit");
+    const startButton = document.getElementById('startButton');
+    const textContainer = document.getElementById('textContainer');
+    const finishButton = document.getElementById('finishButton');
+    const questionnaire = document.getElementById('questionnaire');
+    const quizForm = document.getElementById('quizForm');
+    const results = document.getElementById('results');
+    const questionOptions = document.querySelector('.question-options'); // Contenedor de las opciones de pregunta
+    const timerValue = document.getElementById('timerValue');
+    let time = [];
+
+    let wordCount = 0;
+    let startTime, endTime;
+    let quizCompleted = false;
+    let timerInterval;
+
+    // Texto para las preguntas del cuestionario
+    const questions = [
+        {
+            question: "¿Qué pasaba con la aguja y la mamá de la narradora?", 
+            answers: [
+                "La mamá podía coser lo que quisiera y la aguja lo multiplicaba", 
+                "La mamá no sabía coser, así que se pinchaba el dedo con la aguja", 
+                "Al tocar la aguja, ésta hacía milagros", 
+                "La aguja cosía al bailar la mamá"
+            ], 
+            correctAnswer: "La mamá podía coser lo que quisiera y la aguja lo multiplicaba"
+        },
+        {
+            question: "¿Qué hacía la mamá mientras cosía?", 
+            answers: [
+                "Cantaba y bailaba", 
+                "Hacía de comer a la familia", 
+                "Contaba cada puntada", 
+                "Hablaba con su hija"
+            ], 
+            correctAnswer: "Contaba cada puntada"
+        },
+        {
+            question: "¿Cuál fue la lección aprendida por la mamá?", 
+            answers: [
+                "Nunca más tener afán por coser los deseos de otros", 
+                "No quedarse dormida mientras cose", 
+                "Mejor no coser tanto para no terminar cansada", 
+                "Cobrar la medida justa por cada costura"
+            ], 
+            correctAnswer: "Nunca más tener afán por coser los deseos de otros"
+        },
+        {
+            question: "¿Qué le sucedió a la mamá de tanto coser?", 
+            answers: [
+                "Se olvidó de su familia", 
+                "Se sumergió tanto en su trabajo que dejó de comer y dormir", 
+                "Tuvo mucho éxito y prestigio", 
+                "Todas las respuestas son correctas"
+            ], 
+            correctAnswer: "Todas las respuestas son correctas"
+        },
+        {
+            question: "¿Qué pasó con su familia?", 
+            answers: [
+                "No se dieron por vencidos y la ayudaron a salir de su situación", 
+                "Se pusieron a llorar", 
+                "Se fueron de la casa", 
+                "La buscaron en el barrio"
+            ], 
+            correctAnswer: "No se dieron por vencidos y la ayudaron a salir de su situación"
+        }
+    ];
+            
+    // Función para contar las palabras en el texto
+    function countWords(text) {
+        return text.split(/\s+/).length;
+    }
+
+    // Función para calcular la velocidad de lectura
+    function calculateReadingSpeed(startTime, endTime, wordCount) {
+        const minutes = (endTime - startTime) / 60000; // Convertir a minutos
+        return Math.round(wordCount / minutes);
+    }
+
+    // Función para mostrar las preguntas del cuestionario
+    function displayQuestions() {
+        const questionList = document.getElementById('questionList');
+        questionList.innerHTML = '';
+        
+        questions.forEach((question, index) => {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `
+                <p>${question.question}</p>
+                <div class="question-options">
+                    ${question.answers.map((answer, i) => `
+                        <label>
+                            <input type="radio" name="answer${index}" value="${answer}">
+                            ${answer}
+                        </label>
+                    `).join('')}
+                </div>
+            `;
+            questionList.appendChild(listItem);
+        });
+    }
+
+    // Evento para comenzar el test al hacer clic en "Empezar"
+    startButton.addEventListener('click', function() {
+        textContainer.classList.remove('hidden');
+        paragraphInit.classList.add("hidden");
+        startButton.classList.add('hidden');
+        startTime = Date.now();
+        // Iniciar el contador
+        timerInterval = setInterval(updateTimer, 1000);
+    });
+
+    // Función para actualizar el contador de tiempo
+    function updateTimer() {
+        const currentTime = Math.floor((Date.now() - startTime) / 1000); // Tiempo transcurrido en segundos
+        timerValue.textContent = currentTime;
+        time.push(currentTime);
+    }
+
+    // Evento para finalizar el test al hacer clic en "Terminar Test"
+    finishButton.addEventListener('click', function() {
+        clearInterval(timerInterval); // Detener el contador
+        titleInit.classList.add("hidden");
+        textContainer.classList.add('hidden');
+        finishButton.classList.add('hidden');
+        endTime = Date.now();
+        wordCount = countWords(document.getElementById('textToRead').textContent);
+        const readingSpeed = calculateReadingSpeed(startTime, endTime, wordCount);
+        questionnaire.classList.remove('hidden');
+        displayQuestions();
+    });
+
+    // Evento para enviar el cuestionario
+    quizForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        if (!quizCompleted) {
+            let anyAnswerSelected = true; // Inicialmente, asumimos que al menos una opción está seleccionada
+
+            questions.forEach((question, index) => {
+                const selectedAnswer = document.querySelector(`input[name="answer${index}"]:checked`);
+                if (!selectedAnswer) { // Si ninguna opción está seleccionada para alguna pregunta
+                    anyAnswerSelected = false;
+                    return;
+                }
+            });
+
+            if (!anyAnswerSelected) {
+                alert("Debes seleccionar una opción para cada pregunta antes de terminar.");
+                return;
+            }
+
+            let correctAnswers = 0;
+            let totalQuestions = questions.length;
+            
+            questions.forEach((question, index) => {
+                const selectedAnswer = document.querySelector(`input[name="answer${index}"]:checked`);
+                if (selectedAnswer && selectedAnswer.value === question.correctAnswer) {
+                    correctAnswers++;
+                }
+            });
+            
+            let comprehensionPercentage = (correctAnswers / totalQuestions) * 100;
+            let timeResult = time[time.length-1];
+            
+            quizCompleted = true;
+            results.classList.remove('hidden');
+            questionnaire.classList.add('hidden');
+            document.getElementById('wordCountValue').textContent = wordCount;
+            document.getElementById('readingSpeedValue').textContent = `${calculateReadingSpeed(startTime, endTime, wordCount)}`;
+            document.getElementById('comprehensionValue').textContent = `${comprehensionPercentage}`;
+            document.getElementById('timeResultValue').textContent = `${timeResult}`;
+            document.getElementById('results').innerHTML += '<p class="lastParagrah">Toma nota de tu velocidad de lectura para poder realizar ajustes en los próximos ejercicios.</p><p class="finalMessage">Puedes salir y pasar a la siguiente clase.</p>'
+            ;
+
+            
+            
+        }
+    });
+
+});
